452615bfaf22485050f9331513446b23addee69b
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-20 11:29:14 -0700

    src/main: finish gradient

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
-use std::io::prelude::*;
-
 mod pixel;
 mod ray;
+use pixel::Pixel;
+use ray::Ray;
+
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
+use nalgebra::Point3;
+use nalgebra::Vector3;
+use std::io::prelude::*;
+
+fn ray_color(ray: Ray) -> Pixel {
+    let unit_direction = ray.direction.normalize();
+    let t = 0.5 * (unit_direction[1] + 1.0);
+    let color = (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0);
+    color.into()
+}
 
 fn main() -> Result<(), anyhow::Error> {
     const WIDTH: u64 = 200;
@@ -10,6 +21,11 @@ fn main() -> Result<(), anyhow::Error> {
 
     let mut file = std::fs::File::create("image.ppm")?;
     write!(file, "P3\n{} {}\n255\n", WIDTH, HEIGHT)?;
+
+    let lower_left_corner = Vector3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vector3::new(4.0, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, 2.0, 0.0);
+
     (0..HEIGHT)
         .rev()
         .flat_map(|y| (0..WIDTH).map(move |x| (x, y)))
@@ -19,7 +35,10 @@ fn main() -> Result<(), anyhow::Error> {
             ProgressBar::new(HEIGHT * WIDTH).with_style(s)
         })
         .map(|(x, y)| {
-            pixel::Pixel::new(x as f64 / WIDTH as f64, y as f64 / HEIGHT as f64,0.2)
+            let u = x as f64 / WIDTH as f64;
+            let v = y as f64 / HEIGHT as f64;
+            let r = Ray::new(Point3::origin(), lower_left_corner + u * horizontal + v * vertical);
+            ray_color(r)
         })
         .map(|px| file.write(px.to_ppm_color().as_bytes()).map(drop))
         .collect::<Result<_, _>>()?;
