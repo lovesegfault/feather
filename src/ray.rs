c178ad6a580a41d127321d29df0f2da635208602
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-20 11:29:00 -0700

    src/ray: convert origin to Point3

--- src/ray.rs
+++ src/ray.rs
@@ -1,12 +1,17 @@
+use nalgebra::Point3;
 use nalgebra::Vector3;
 
 pub struct Ray {
-    pub origin: Vector3<f64>,
+    pub origin: Point3<f64>,
     pub direction: Vector3<f64>,
 }
 
 impl Ray {
-    pub fn at(&self, t: f64) -> Vector3<f64> {
+    pub fn new(origin: Point3<f64>, direction: Vector3<f64>) -> Self {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Point3<f64> {
         self.origin + (self.direction * t)
     }
 }
