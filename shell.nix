7caf65f2122a95b801133f9ea9b73c0a4e11b473
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-20 16:41:56 -0700

    shell: cleanup

--- shell.nix
+++ shell.nix
@@ -8,35 +8,14 @@ pkgs.mkShell {
     cmake
     pkgconfig
 
-    flamegraph
-    linuxPackages_latest.perf
     rust-analyzer
     cargo-edit
+    cargo-tree
+
+    cargo-flamegraph
+    linuxPackages_latest.perf
+
     niv
     nixpkgs-fmt
   ];
-
-  buildInputs = with pkgs; [
-    alsaLib
-    SDL2
-    libGL
-    libxkbcommon
-    wayland
-    xlibs.libX11
-  ];
-
-  APPEND_LIBRARY_PATH = with pkgs; stdenv.lib.makeLibraryPath [
-    SDL2
-    libGL
-    vulkan-loader
-    wayland
-    xlibs.libXcursor
-    xlibs.libXi
-    xlibs.libXrandr
-    libxkbcommon
-  ];
-
-  shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
-  '';
 }
